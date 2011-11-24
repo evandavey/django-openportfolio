5737a35c5031b1b562629ac5812aef6ceffff382
Evan Davey <evan.j.davey@gmail.com>
2011-11-24 15:40:16 +0000

    moving subsclassqs into its own module

--- openportfolioapp/models/__init__.py
+++ openportfolioapp/models/__init__.py
@@ -14,3 +14,4 @@ from datafiles import *
 from interestrate import InterestRate
 from returnobj import Return
 from returns import *
+from SubclassingQuerySet import SubclassingQuerySet
--- openportfolioapp/models/price.py
+++ openportfolioapp/models/price.py
@@ -1,17 +1,6 @@
 from django.db import models
 from django.contrib.contenttypes.models import ContentType
-from django.db.models.query import QuerySet
-
-class SubclassingQuerySet(QuerySet):
-    def __getitem__(self, k):
-        result = super(SubclassingQuerySet, self).__getitem__(k)
-        if isinstance(result, models.Model) :
-            return result.as_leaf_class()
-        else :
-            return result
-    def __iter__(self):
-        for item in super(SubclassingQuerySet, self).__iter__():
-            yield item.as_leaf_class()
+from openportfolioapp.models import SubclassingQuerySet
 
 class PriceManager(models.Manager):
     def get_query_set(self):
