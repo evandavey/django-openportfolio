d5a7853ae63ab19810758cd9540fd17fb794d058
Evan Davey <evan.j.davey@gmail.com>
2011-11-24 14:56:13 +0000

    updated staging to use mysql

--- settings_staging.py
+++ settings_staging.py
@@ -5,5 +5,9 @@ DEBUG = True
 
 STATIC_ROOT = '/usr/local/web/django/www/staging/openportfolio/static'
 
-DATABASE_ENGINE = 'sqlite3'
-DATABASE_NAME = '/usr/local/web/django/db/openportfolio.db'
+DATABASE_ENGINE = 'mysql'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+DATABASE_NAME = 'staging_openportfolio'             # Or path to database file if using sqlite3.
+DATABASE_USER = 'openportfolio'             # Not used with sqlite3.
+DATABASE_PASSWORD = 'openportfolio'         # Not used with sqlite3.
+DATABASE_HOST = 'localhost'             # Set to empty string for localhost. Not used with sqlite3.
+DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
