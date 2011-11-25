ad58ff59a0f8b067cadbe8fe82f00c0c5dc14b99
Evan Davey <evan.j.davey@gmail.com>
2011-11-25 12:23:48 +0000

    cleans currency model

--- openportfolioapp/models/currency.py
+++ openportfolioapp/models/currency.py
@@ -2,7 +2,6 @@ from django.db import models
 from openportfolioapp.utils.currencyhistory import *
 import sys
 from openportfolioapp.models.prices import CurrencyPrice
-import numpy as np
 
 class Currency(models.Model):
     code=models.CharField(max_length=6,primary_key=True)
@@ -19,13 +18,20 @@ class Currency(models.Model):
 
     @property
     def priceframe(self):
+        """
+        Loads a currency queryset into a pandas dataframe
+        """
         
         return self.currencyprice_set.all().dataframe()
 
 
     def fetch_price_frame(self,startdate,enddate):
 
-
+        """
+        Downloads data from the ukforex website, returning the result
+        as a pandas dataframe
+        """
+        
         try:
 
             df = fetch_ukforex_historical_exchange_rates(startdate,enddate,self.code,'USD')
@@ -35,10 +41,13 @@ class Currency(models.Model):
             print "Error loading uk forex data for %s %s" % (self,str(startdate))
             return None
 
-
         return df
 
     def save_price_frame(self,df):
+        
+        """
+        Saves a currency dataframe object to the database
+        """
 
         if df is None:
             return
@@ -58,46 +67,4 @@ class Currency(models.Model):
 
             p.save()
 
-    def load_price_frame(self,startdate,enddate,curr='USD'):
-
-
-        #if currency is other than USD, load it's frame for crossrate calculation
-        crosscurr=None
-        if curr != 'USD':
-            try:
-                crosscurr=Currency.objects.get(code=curr)
-
-                pdf_cross=crosscurr.load_price_frame(startdate,enddate,'USD')
-            except:
-                print 'Could not load %s, defaulting to USD' % curr
-                crosscurr=None
-                curr='USD'
-
-        #load prices into a numpy array
-        if startdate is None:
-            qs=CurrencyPrice.objects.filter(date__lte=enddate,currency=self)
-        else:
-            qs=CurrencyPrice.objects.filter(date__lte=enddate,date__gte=startdate,currency=self)
-
-        vlqs = qs.values_list()
-        prices = np.core.records.fromrecords(vlqs, names=[f.name for f in CurrencyPrice._meta.fields])
-        dates = [datetime.combine(d,time()) for d in prices.date]
-
-
-        data={
-            'crossrate': prices.price,
-            }
-
-        pdf=ps.DataFrame(data,index=dates)	
-
-        #perform cross currency calcs
-        if crosscurr is not None:
-            pdf_cross=pdf_cross.reindex(dates)
-
-        data={
-        'crossrate': pdf['crossrate']/pdf_cross['crossrate']
-        }
-
-        pdf=ps.DataFrame(data,index=dates)	
-
-        return pdf
+ 
