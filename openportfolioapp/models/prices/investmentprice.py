e5fd5bc1eaf6c62c9e3ae845d2b90db65e3a85e9
Evan Davey <evan.j.davey@gmail.com>
2011-11-27 22:05:11 +0000

    fixes indenting bug on save

--- openportfolioapp/models/prices/investmentprice.py
+++ openportfolioapp/models/prices/investmentprice.py
@@ -57,11 +57,11 @@ class InvestmentPrice(Price):
 		app_label = "openportfolioapp"
 		
 
-	def save(self, *args, **kwargs):
-		if(not self.content_type):
-			self.content_type = ContentType.objects.get_for_model(self.__class__)
-		
-		super(InvestmentPrice, self).save(*args, **kwargs)
+    def save(self, *args, **kwargs):
+        if(not self.content_type):
+            self.content_type = ContentType.objects.get_for_model(self.__class__)
+
+        super(InvestmentPrice, self).save(*args, **kwargs)
 
 	def as_leaf_class(self):
 		content_type = self.content_type
