f9e5f8c9a82f5c4b2cd25561a5fcab3acf486176
Evan Davey <evan.j.davey@gmail.com>
2011-11-28 13:23:28 +0000

    removes ref to portfolio price

--- openportfolioapp/models/prices/__init__.py
+++ openportfolioapp/models/prices/__init__.py
@@ -1,5 +1,4 @@
 from listedequityprice import ListedEquityPrice
 from currencyprice import CurrencyPrice
 from savingsaccountprice import SavingsAccountPrice
-from portfolioprice import PortfolioPrice
 from investmentprice import InvestmentPrice
