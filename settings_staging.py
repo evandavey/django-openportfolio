a0bf25feeddda067c8f6c3a55123740637f7a7ac
Evan Davey <evan.j.davey@gmail.com>
2011-11-28 19:37:05 +0000

    added template dirs

--- settings_staging.py
+++ settings_staging.py
@@ -6,6 +6,8 @@ DEBUG = True
 STATIC_ROOT = '/usr/local/web/django/www/staging/openportfolio/static'
 MEDIA_ROOT = '/usr/local/web/django/www/staging/openportfolio/media'
 
+TEMPLATE_DIRS = ('/usr/local/web/django/www/staging/openportfolio/openportfolioapp/templates')
+
 
 DATABASE_ENGINE = 'mysql'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
 DATABASE_NAME = 'staging_openportfolio'             # Or path to database file if using sqlite3.
