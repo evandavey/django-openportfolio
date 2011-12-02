b408c580370df832b451aaec5fe817eca8e3edd7
Evan Davey <evan.j.davey@gmail.com>
2011-12-02 12:13:53 +0000

    adds media root setting

--- settings_development.py
+++ settings_development.py
@@ -2,6 +2,8 @@ from openportfolio.settings import *
 
 DEBUG = True
 
+
+MEDIA_ROOT = "media"
 TEMPLATE_DIRS=('/Users/evandavey/Documents/openportfolio/openportfolioapp/templates/')
 
 DATABASE_ENGINE = 'sqlite3'
