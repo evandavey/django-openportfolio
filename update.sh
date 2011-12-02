569bca13f2fe4d3e6be4bfc37c380c545ef6401d
Evan Davey <evan.j.davey@gmail.com>
2011-12-02 12:12:47 +0000

    update tools moved to tools

--- update.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-source /usr/local/web/django/www/production/env/openportfolio/bin/activate
-/usr/local/web/django/www/production/openportfolio/manage.py openportfolio-update --settings=openportfolio.settings_production
